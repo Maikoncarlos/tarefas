89a76ff322f9bfba1035988e72a90f9c868daf77
Maikoncarlos <maikonsaturno@gmail.com>
2022-04-14 15:04:44 -0300

    personalizando e padronizando respostas de error

--- src/main/java/com/github/maikoncarlos/tarefas/exception/TarefaExceptionHandler.java
+++ src/main/java/com/github/maikoncarlos/tarefas/exception/TarefaExceptionHandler.java
@@ -8,11 +8,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Date;
+
 @ControllerAdvice
 public class TarefaExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler
+    @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<Object> handlerAnyException(Exception e, WebRequest request){
-        return new ResponseEntity<>(e, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+        String descricaoError = e.getLocalizedMessage();
+        if(descricaoError == null) descricaoError = e.toString();
+        ErrorMsg errorMsg = new ErrorMsg(new Date(), descricaoError);
+        return new ResponseEntity<>(errorMsg, new HttpHeaders(), HttpStatus.NO_CONTENT);
     }
 }
