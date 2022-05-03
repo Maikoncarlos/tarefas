60d1e8562052d0f171feadcd40a85c7f66e1bec8
Maikoncarlos <maikonsaturno@gmail.com>
2022-05-03 14:49:32 -0300

    deixando nomenclatura padronizada

--- src/main/java/com/github/maikoncarlos/tarefas/repositories/TarefaRepository.java
+++ src/main/java/com/github/maikoncarlos/tarefas/repositories/TarefaRepository.java
@@ -1,9 +1,9 @@
 package com.github.maikoncarlos.tarefas.repositories;
 
-import com.github.maikoncarlos.tarefas.entities.Tarefa;
+import com.github.maikoncarlos.tarefas.entities.TarefaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+public interface TarefaRepository extends JpaRepository<TarefaEntity, Long> {
 }
