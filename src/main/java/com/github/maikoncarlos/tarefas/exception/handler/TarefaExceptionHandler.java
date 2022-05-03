d591bf4dda9f6ca1ac66c8778a0f9e9ba0a64d8d
Maikoncarlos <maikonsaturno@gmail.com>
2022-05-03 12:03:49 -0300

    padronizando todas as excessões

--- src/main/java/com/github/maikoncarlos/tarefas/exception/handler/TarefaExceptionHandler.java
+++ src/main/java/com/github/maikoncarlos/tarefas/exception/handler/TarefaExceptionHandler.java
@@ -3,16 +3,18 @@ package com.github.maikoncarlos.tarefas.exception.handler;
 import com.github.maikoncarlos.tarefas.exception.NotFoundTaskException;
 import com.github.maikoncarlos.tarefas.exception.StandardError;
 import com.github.maikoncarlos.tarefas.exception.ValidErrors;
-import org.springframework.beans.BeanInstantiationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
@@ -22,7 +24,7 @@ import java.util.stream.Collectors;
 
 @ControllerAdvice
 @RestController
-public class TarefaExceptionHandler {
+public class TarefaExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Autowired
     private MessageSource messageSource;
@@ -38,6 +40,40 @@ public class TarefaExceptionHandler {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 
+    @Override
+    public final ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException manvException,
+                                                                     HttpHeaders headers,
+                                                                     HttpStatus status,
+                                                                     WebRequest request) {
+        List<FieldError> fieldErrors = manvException.getBindingResult().getFieldErrors();
+        String field = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining("  /  "));
+        String fieldMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining("  /  "));
+
+        ValidErrors error = new ValidErrors();
+        error.setTimestamp(Instant.now());
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setError("Not value is present");
+        error.setMessage(manvException.toString());
+        error.setPath(request.getContextPath());
+        error.setField(field);
+        error.setFieldMessage(fieldMessage);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex,
+                                                             @Nullable Object body,
+                                                             HttpHeaders headers,
+                                                             HttpStatus status,
+                                                             WebRequest request) {
+        StandardError error = new StandardError();
+        error.setTimestamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("Not value is present");
+        error.setMessage(ex.getMessage());
+        error.setPath(request.getDescription(true));
+        return new ResponseEntity(error, headers, status);
+    }
+
     @ExceptionHandler(NotFoundTaskException.class)
     public final ResponseEntity<StandardError> handleNoContentException(Exception ex, HttpServletRequest request) {
         StandardError error = new StandardError();
@@ -49,21 +85,5 @@ public class TarefaExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public final ResponseEntity<ValidErrors> handlerException(MethodArgumentNotValidException manvException, HttpServletRequest request) {
-        List<FieldError> fieldErrors = manvException.getBindingResult().getFieldErrors();
-       String field = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining("  /  "));
-        String fieldMessage =  fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining("  /  "));
-
-        ValidErrors error = new ValidErrors();
-        error.setTimestamp(Instant.now());
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setError("Not value is present");
-        error.setMessage(manvException.toString());
-        error.setPath(request.getRequestURI());
-        error.setField(field);
-        error.setFieldMessage(fieldMessage);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
-    }
 
 }
